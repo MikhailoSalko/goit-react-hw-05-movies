826e39740df7dca5aa52506fe1f5d605025ac5d4
Mikhailo Salko <slipknotmisha@gmail.com>
2023-06-03 19:15:56 +0300

    add some features to movieDetails

--- src/pages/MoviesDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MoviesDetailsPage/MovieDetailsPage.jsx
@@ -5,21 +5,45 @@ import { StyledBackButton } from 'styles/pageStyles.styled';
 // import { Report } from 'notiflix';
 
 const MovieDetailsPage = () => {
-  const [film, setFilm] = useState([]);
+  const [film, setFilm] = useState({});
   const { movieId } = useParams();
   const location = useLocation();
   const navigate = useNavigate();
-  console.log(movieId);
-  // const obj = { overview, poster_path, title, gernes, vote_average };
+
+  // {
+  //   overview, poster_path, title, genres, vote_average;
+  // }
 
   useEffect(() => {
     fetchFilmById(movieId).then(data => {
       console.log(data);
-      // setFilm({ image: data });
+      setFilm({ ...data });
     });
     // .catch(error => setError(error.message))
-    // .finally(() => setLoading(false));;
+    // .finally(() => setLoading(false));
   }, [movieId]);
+  console.log(film);
+
+  // console.log(film);
+  // console.log(film.genres);
+
+  // const array = Object.entries(film.genres).forEach(([_, name]) =>
+  //   console.log(name.name)
+  // );
+
+  // const array = Object.entries(film.genres);
+  // console.log(array);
+
+  // const { overview, image, genres, vote_average, title } = film;
+  // console.log(genres);
+  // const arrayOfGenres = [];
+  // genres.forEach(obj => arrayOfGenres.push(obj.name));
+  // console.log(genres[0].name);
+  // console.log(genres[1].name);
+  // console.log(genres[2].name);
+
+  // const genresToRend = Object.fromEntries([...genres]);
+  // console.log(genresToRend);
 
   return (
     <>
@@ -30,13 +54,13 @@ const MovieDetailsPage = () => {
         Go Back
       </StyledBackButton>
       <div>
-        <img src="" alt="" />
-        <h1>title</h1>
-        <p>User Score: </p>
+        {/* <img src={image} alt="" />
+        <h1>{title}</h1>
+        <p>User Score: {parseInt(vote_average * 10)}</p>
         <h3>Overview</h3>
-        <p></p>
-        <h3>Genres</h3>
-        <p></p>
+        <p>{overview}</p>
+        <h3>Genres</h3> */}
+        {/* <p>{((genres[0].name, genres[1].name), genres[2].name)}</p> */}
       </div>
       <br />
       <h2>Additional information</h2>
